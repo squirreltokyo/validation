c07f8773cd50699e3304d8df8e93ac98f2216409
pleasetellme@163.com <pleasetellme@163.com>
2020-12-03 23:09:35 +0900

    validation exam for flutter

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Form Validation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  void validate() {
+    if (formkey.currentState.validate()) {
+      print("Validated");
+    } else {
+      print("Not Validated");
+    }
+  }
+
+  String validatepass(value) {
+    if (value.isEmpty) {
+      return "Require Password";
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Someapp'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Center(
+          child: Form(
+            //always and disabled   different shows to ui
+            autovalidateMode: AutovalidateMode.always,
+            key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                    decoration: InputDecoration(labelText: "Email"),
+                    validator: MultiValidator([
+                      RequiredValidator(errorText: "Require Email"),
+                      EmailValidator(errorText: "Not a Valid Email"),
+                    ])),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: "password"),
+                    validator: validatepass,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: RaisedButton(
+                    onPressed: validate,
+                    child: Text("Login"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
